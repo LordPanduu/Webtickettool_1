cc45bfcaabc87a85dd7bf05b8368cfbea9e5c9fa
LordPanduu <72973276+LordPanduu@users.noreply.github.com>
2020-11-18 13:32:58 +0100

    Update

--- Webticketing_tool/WEbtool1/urls.py
+++ Webticketing_tool/WEbtool1/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = {
     path('', views.hi, name="home-page"),
     path('login', views.login, name="login-page"),
+    path("dashboard", views.dashboard, name="dashboard-page"),
 }
--- Webticketing_tool/WEbtool1/views.py
+++ Webticketing_tool/WEbtool1/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, request
+
+tickets = [
+    {
+        "author": "Elias",
+        "title": "network connection failed",
+        "content": "I cant connect to the server share",
+        "post_date": "18.11.2020"
+    },
+    {
+        "author": "Elias",
+        "title": "network connection failed",
+        "content": "I cant connect to the server share",
+        "post_date": "18.11.2020"
+    }
+]
 # Create your views here.
 def hi(request):
     return render(request, "Webtool/hi.html")
@@ -8,3 +23,9 @@ def hi(request):
 
 def login(request):
     return render(request, "Login/login.html")
+
+def dashboard(reguest):
+    context = {
+        "tickets": tickets
+    }
+    return render(request, "Dashboard/skel_js/dashboard.html")
